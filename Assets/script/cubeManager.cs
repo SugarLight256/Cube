ed01de6041cb240402df2f09cb9e17c128e0b909
SugarLight256 <nippen12@gmail.com>
2016-01-20 16:21:52 +0900

    Place Cube object

---
+++ Assets/script/cubeManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeManager : MonoBehaviour {
+
+    public static int boxSize;
+    private int cubeCount;
+    public GameObject[] cubes;
+
+	// Use this for initialization
+	void Start () {
+        boxSize = 3;
+        cubeCount = boxSize * boxSize * boxSize;
+        cubes = new GameObject[cubeCount];
+	    for(int i=0; i<cubeCount; i++)
+        {
+            cubes[i] = transform.GetChild(i).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
